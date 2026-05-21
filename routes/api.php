8865646591a4dbd7532377b14d33eaebe836055d
Arief Raihandi Azka <raihandi93@gmail.com>
2026-05-22 01:05:30 +0700

    un

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TelegramController;
 use App\Http\Controllers\PaymentController;
 
+// Jalur Webhook Telegram
 Route::post('/telegram/webhook', [TelegramController::class, 'handle']);
 
+// Jalur Callback iPaymu
 Route::post('/ipaymu/callback', [PaymentController::class, 'callback']);
