bc5a3c7fc32d8f27d8892d478a8b7456b704cea0
Arief Raihandi Azka <raihandi93@gmail.com>
2026-05-23 05:53:47 +0700

    jgh

--- database/migrations/2026_05_22_185223_create_payments_table.php
+++ database/migrations/2026_05_22_185223_create_payments_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('telegram_id')->unique();
+            $table->bigInteger('telegram_id');
             $table->string('username')->nullable();
             $table->string('name')->nullable();
             $table->string('package')->nullable();
